da569533da87b55d6352ba6930abb4d704f69d61
Fletcher Nichol <fnichol@nichol.ca>
2012-08-20 22:42:28 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Data bag driven application deployment."
 long_description "Please refer to README.md (it's long)."
-version          "0.1.2"
+version          "0.1.3"
 
 supports "ubuntu"
 
